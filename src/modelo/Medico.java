e2d18ff9c1be588bb6560d2d404ba4b989c1bfea
kevinmendoza2001 <shaka123kamm@gmail.com>
2026-05-26 21:44:44 -0500

    Nuevo commit

--- src/modelo/Medico.java
+++ src/modelo/Medico.java
@@ -5,6 +5,7 @@ public class Medico extends Empleado {
     private int numeroPacientesAtendidos;
     private double valorConsulta;
 
+    // Utilizamos el Super para indicar que vamos a usar todo el contenido dentro de esta clase
     public Medico(String cedula, String nombre, int edad, String telefono, String correo,
                   String especialidad, int numeroPacientesAtendidos, double valorConsulta) {
         super(cedula, nombre, edad, telefono, correo);
@@ -13,11 +14,12 @@ public class Medico extends Empleado {
         this.valorConsulta = valorConsulta;
     }
 
+    // Utilizamos sobreEscritura para realizar los calculos correspondientes
     @Override
     public double calcularPago() {
         return numeroPacientesAtendidos * valorConsulta;
     }
-
+    // Sobreescribimos para que se vaya autoincrementando los datos al imprimir el metodo
     @Override
     public void mostrarInformacion() {
         super.mostrarInformacion();
